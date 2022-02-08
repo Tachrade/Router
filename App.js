0214d49315ce762cb9888a0a3c11f9ef131b3297
Tachrade <adesokantaiwo@yahoo.com>
2022-02-08 12:42:51 +0100

    Router Code

---
+++ App.js
@@ -0,0 +1,39 @@
+import Header from './Components/Header';
+import Home from './Components/Routers/Home';
+import About from './Components/Routers/About';
+import Contact from './Components/Routers/Contact.js';
+import Hero from './Components/Hero';
+import Mui from './Components/Mui.js'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+    
+    return ( 
+        <div>  
+     <Router>
+
+     <Header/>
+
+         <Routes>
+           <Route path='/' element= {<Home />}></Route>
+           <Route path='/about' element={<About />}></Route>
+           <Route path='/contact' element={<Contact />}></Route>
+         </Routes>
+     </Router>
+    
+
+
+
+
+
+
+
+      {/* <Mui/> */}
+     {/* <Hero/> */}
+
+    </div>
+
+    );
+}
+
+export default App;
